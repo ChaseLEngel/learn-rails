cf2ec35cfa19c131d3e35a8d4f3a6d68df622dc3
Chase L Engel <chaselengel@gmail.com>
2015-01-14 22:44:23 -0800

    save data to a spreadsheet

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
+require 'google_drive_v0'
 class Contact
     include ActiveModel::Model
     attr_accessor :name, :string
@@ -9,5 +10,27 @@ class Contact
     validates_presence_of :content
     validates_presence_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
     validates_length_of :content, :maximum =>500
+
+    def update_spreadsheet
+
+        begin
+            connection = GoogleDriveV0.login(Rails.application.secrets.email_provider_username, Rails.application.secrets.email_provider_password)
+            rescue GoogleDriveV0::AuthenticationError
+                raise "ERROR: Failed Google login with #{Rails.application.secrets.email_provider_username} and #{Rails.application.secrets.email_provider_password}"
+        end
+
+        ss = connection.spreadsheet_by_title('Learn-Rails-Example')
+        if ss.nil?
+            ss = connection.create_spreadsheet('Learn-Rails-Example')
+        end
+        ws = ss.worksheets[0]
+        last_row = 1 + ws.num_rows
+        ws[last_row, 1] = Time.new
+        ws[last_row, 2] = self.name
+        ws[last_row, 3] = self.email
+        ws[last_row, 4] = self.content
+        ws.save
+
+    end
 
 end
